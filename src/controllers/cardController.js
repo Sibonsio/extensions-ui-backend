1bc14e219ae3cb3c9ad025723ff4d21faa792144
Sibonsio <siboniso1sekute@gmail.com>
2025-11-13 05:24:17 +0200

    routes created

---
+++ src/controllers/cardController.js
@@ -0,0 +1,14 @@
+
+
+export const getAllCards = async (req, res) => {
+
+}
+export const getActiveCards = async (req, res) => {
+
+}
+export const getInactiveCards = async (req, res) => {
+
+}
+export const createCard = async (req, res) => {
+
+}
--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { getAllCards, getActiveCards, getInactiveCards, createCard } from "../controllers/cardController.js";
+
 
 const cardRoutes = Router();
 
-
-
-
+cardRoutes.get('/all', getAllCards)
+cardRoutes.get('/active', getActiveCards)
+cardRoutes.get('/inactive', getInactiveCards)
+cardRoutes.post('/create', createCard)
 
 export default cardRoutes
