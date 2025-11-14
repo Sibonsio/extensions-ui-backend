f5c77fb8d22cb2f2c01f87dc2398383690e71409
Sibonsio <siboniso1sekute@gmail.com>
2025-11-14 23:24:12 +0200

    updated route.jsx

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -4,12 +4,21 @@ import multer from "multer";
 
 
 const cardRoutes = Router();
-const upload = multer();
+const uploadPath = process.env.PATH
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadPath)
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage });
 
 cardRoutes.get('/all', getAllCards)
 cardRoutes.get('/active', getActiveCards)
 cardRoutes.get('/inactive', getInactiveCards)
-cardRoutes.post('/create', upload.single(), createCard)
+cardRoutes.post('/create', upload.single('image'), createCard)
 cardRoutes.delete('/delete/:id', deleteCard)
 cardRoutes.patch('/update/:id', updateCardById)
 
