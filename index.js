974058400775691cff71d637fdbf0e4722dc7742
Sibonsio <siboniso1sekute@gmail.com>
2025-11-14 19:35:45 +0200

    changes

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import multer from 'multer'
 import connectDB from './src/config/db.js'
 import cardRoutes from './src/routes/cardRoutes.js'
 import corsOptions from './src/config/cors.js'
 import dotenvConfig from './src/config/env.js'
 
 const app = express()
-const upload = multer()
 dotenvConfig()
 await connectDB()
 app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(upload.array())
 app.use('/api/v1', cardRoutes)
-
 app.listen(process.env.PORT, () => {
     console.log(`The server is running on: http://localhost:${process.env.PORT}`)
 })
--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { getAllCards, getActiveCards, getInactiveCards, createCard, updateCardById, deleteCard } from "../controllers/cardController.js";
+import multer from "multer";
 
 
 const cardRoutes = Router();
+const upload = multer();
 
 cardRoutes.get('/all', getAllCards)
 cardRoutes.get('/active', getActiveCards)
 cardRoutes.get('/inactive', getInactiveCards)
-cardRoutes.post('/create', createCard)
+cardRoutes.post('/create', upload.single(), createCard)
 cardRoutes.delete('/delete/:id', deleteCard)
 cardRoutes.patch('/update/:id', updateCardById)
 
