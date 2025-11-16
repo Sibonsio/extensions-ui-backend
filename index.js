481fb04200db3e62583cf560a97a0032db163c6a
Sibonsio <siboniso1sekute@gmail.com>
2025-11-16 08:45:22 +0200

    project complete

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ import cardRoutes from './src/routes/cardRoutes.js'
 import corsOptions from './src/config/cors.js'
 import dotenvConfig from './src/config/env.js'
 
+
 const app = express()
 dotenvConfig()
 await connectDB()
 app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use('/public', express.static('public'))
 app.use('/api/v1', cardRoutes)
 app.listen(process.env.PORT, () => {
     console.log(`The server is running on: http://localhost:${process.env.PORT}`)
